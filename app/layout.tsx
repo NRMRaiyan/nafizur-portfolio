cfe59b709838f953d3437ac33ab540d6172b3f95
NRMRaiyan <nrmraiyan@gmail.com>
2026-08-31 23:58:30 +0600

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Space_Grotesk, Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next"
 import './globals.css';
 
 const space = Space_Grotesk({ subsets: ['latin'], variable: '--font-space', display: 'swap' });
